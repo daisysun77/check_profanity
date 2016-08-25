9f686c52980384b920b4cf82ed74b5ac520b2ec0
daisysun77 <stella.sundq@gmail.com>
2016-08-25 13:10:33 -0400

    Update check_profanity.py
    Update comments for compliance to the Google Python Style Guide.

--- check_profanity.py
+++ check_profanity.py
@@ -1,14 +1,14 @@
 import urllib
 
 def read_text():
-    quotes = open("{{text file}}")      #open a text file
-    contents_of_file = quotes.read()      #read text from the file
+    quotes = open("{{text file}}")
+    contents_of_file = quotes.read()
     #print(contents_of_file)
     quotes.close()
-    check_profanity(contents_of_file)     #check profanity
+    check_profanity(contents_of_file)
 
 def check_profanity(text_to_check):
-    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)     #Google-what do you like service
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)     # Google-what do you like service
     output = connection.read()
     #print(output)
     connection.close()
